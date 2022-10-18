b9571cc4cfeacf2158e7523f8f3dc673e5f6408d
Kevin Li <likevinsen@gmail.com>
2022-10-17 21:18:10 -0500

    stuff

--- crumbs/crumbs/FeedViewController.swift
+++ crumbs/crumbs/FeedViewController.swift
@@ -7,16 +7,55 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
+class PostTableViewCell : UITableViewCell {
+    
+}
+
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var cardTable: UITableView!
+    private final let DISCOVER_IDX = 0
+    private final let FOLLOW_IDX = 1
+    private final let CARD_IDENTIFIER = "PostCardIdentifier"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.setHidesBackButton(true, animated: true)
-
-        // Do any additional setup after loading the view.
+        self.cardTable.delegate = self
+        self.cardTable.dataSource = self
+        
+        self.cardTable.rowHeight = UITableView.automaticDimension
+        self.cardTable.estimatedRowHeight = 1000
     }
     
 
+    @IBAction func changedSegment(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == DISCOVER_IDX {
+            view.backgroundColor = .red
+            
+        } else if sender.selectedSegmentIndex == FOLLOW_IDX {
+            view.backgroundColor = .blue
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CARD_IDENTIFIER, for: indexPath)
+        // let row = indexPath.row
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.lineBreakMode = .byWordWrapping
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
     /*
     // MARK: - Navigation
 
