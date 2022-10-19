5e18c0f0633d0dc6af6fac1e09bf8ba7a3ee4133
Philo Lin <philolin@kaylee.cs.utexas.edu>
2022-10-18 21:57:29 -0500

    removed button and segue

--- crumbs/crumbs/ViewController.swift
+++ crumbs/crumbs/ViewController.swift
@@ -8,15 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let segueIdentifier = "PostCreationSegueIdentifier"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-    @IBAction func createPostButtonPressed(_ sender: Any) {
-        print("hi")
-        performSegue(withIdentifier: segueIdentifier, sender: self)
-        print("done")
     }
 }
 
