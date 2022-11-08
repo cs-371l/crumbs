37f32f458ec23a36b2db773a114f0861f5e38846
Tristan Blake <tristanb@utexas.edu>
2022-11-07 22:11:35 -0600

    another change

--- crumbs/crumbs/AddCommentViewController.swift
+++ crumbs/crumbs/AddCommentViewController.swift
@@ -7,11 +7,41 @@
 
 import UIKit
 
-class AddCommentViewController: UIViewController {
+class AddCommentViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var commentTextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        commentTextView.delegate = self
+        commentTextView.text = "Add comment"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Comment", style: .done, target: self, action: #selector(self.addComment))
+    }
+    
+    @objc func addComment() {
+        let commentText = commentTextView.text!
         
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "Add comment"
+            textView.textColor = UIColor.lightGray
+        }
     }
 
 }
