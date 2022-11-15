ed7197dfb18790047f4e176bda4746f78f5d603c
Philo Lin <philolin@kakapo.cs.utexas.edu>
2022-11-14 22:31:33 -0600

    added color changes to login/signup and fixed image bug with darkmode

--- crumbs/crumbs/views/feed/FeedViewController.swift
+++ crumbs/crumbs/views/feed/FeedViewController.swift
@@ -54,6 +54,10 @@ class FeedViewController: UIViewController, TableManager {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Create Post", style: .plain, target: self, action: #selector(goToPostCreate))
+        
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Map           ", style: .plain, target: self, action: #selector(goToPostCreate))
+        
+        addNavBarImage()
         // check if dark mode
         let defaults = UserDefaults.standard
         if defaults.bool(forKey: "Dark") {
@@ -62,7 +66,20 @@ class FeedViewController: UIViewController, TableManager {
             UIApplication.shared.keyWindow?.rootViewController?.overrideUserInterfaceStyle = .light
         }
     }
-
+    
+    func addNavBarImage() {
+            let navController = navigationController!
+            let image = UIImage(named: "NavbarLogo.png") //Your logo url here
+            let imageView = UIImageView(image: image)
+            let bannerWidth = navController.navigationBar.frame.size.width
+            let bannerHeight = navController.navigationBar.frame.size.height
+            let bannerX = bannerWidth / 2 - (image?.size.width)! / 2
+            let bannerY = bannerHeight / 2 - (image?.size.height)! / 2
+            imageView.frame = CGRect(x: bannerX, y: bannerY, width: bannerWidth, height: bannerHeight)
+            imageView.contentMode = .scaleAspectFit
+            navigationItem.titleView = imageView
+        }
+    
     @IBAction func changedSegment(_ sender: UISegmentedControl) {
         self.embeddedView.discoverActive = sender.selectedSegmentIndex == DISCOVER_IDX
         updateTable()
